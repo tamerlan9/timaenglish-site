7b5d091b18a80f45266c4d720b5fa7013393b29b
tamerlan9 <tamerlan9@inbox.ru>
2026-01-13 18:00:37 +0600

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,11 @@ export const metadata: Metadata = {
   applicationName: "Tima English",
 
   icons: {
-    icon: "/favicon.png",
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
     apple: "/apple-touch-icon.png",
   },
 
@@ -59,9 +63,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
