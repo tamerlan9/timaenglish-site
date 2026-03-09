76517922c011c431a6fbeb5ffbb0cbbc09f3ddbb
tamerlan9 <tamerlan9@inbox.ru>
2026-03-09 19:15:43 +0600

    1

--- app/delete-account/page.tsx
+++ app/delete-account/page.tsx
@@ -57,9 +57,9 @@ export default function DeleteAccountPage() {
       <section className={sectionCls}>
         <h2 className={h2Cls}>Data we delete</h2>
         <ul className="mt-3 list-disc pl-6 space-y-1 text-zinc-700 dark:text-zinc-300">
-          <li>Account</li>
+          <li>Account information</li>
           <li>Learning progress and statistics</li>
-          <li>Account-related preferences</li>
+          <li>Account preferences</li>
         </ul>
       </section>
 
